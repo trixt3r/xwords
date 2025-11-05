23ca6362f0bdc9aac4b35141a2d60b931e8aed3d
vincent <hamon.vincent@gmail.com>
2025-11-05 11:48:54 +0100

    c'est pas mal

--- src/words/essai.py
+++ src/words/essai.py
@@ -13,22 +13,23 @@ from pprint import pprint
 ####################################################
 ####################################################
 
-def bitmask(length,offset):
+def create_bitmask(length,offset):
     return ((1 << length) - 1) << offset
-    # return ((1 << length) - 1) >> offset
 
 def bitmask_for_group(grps2tokens,gid):
-    
-    return bitmask(len(grps2tokens[gid]).bit_length(), sum(len(grps2tokens[g]).bit_length() for g in range(gid)))
-    
-    # return bitmask(len(grps2tokens[gid]).bit_length(), sum(len(grps2tokens[g]).bit_length() for g in range(gid)))
-    bit_fields_lengths = [len(grp).bit_length() for grp in grps2tokens]
-    total_bits = sum(bit_fields_lengths)
-    mask = (1 << total_bits) - 1
-    return mask
+    if isinstance(gid,list):
+        return(sum(bitmask_for_group(grps2tokens,g) for g in gid))
+    return create_bitmask(len(grps2tokens[gid]).bit_length(), sum(len(grps2tokens[g]).bit_length() for g in range(gid)))
 
 def offset_for_group(grps2tokens,gid):
     return sum(len(grps2tokens[g]).bit_length() for g in range(gid))
+
+def word_to_groups(word:str, grp2tokens)->list[int]:
+    groups = []
+    for gid, tokens in enumerate(grp2tokens):
+        if any(tok in word for tok in tokens):
+            groups.append(gid)
+    return groups
 
 def token_to_bits(token_code, bit_fields_lengths, reverse: bool = True):
     """
@@ -109,13 +110,14 @@ def test_arbo(values_set):
 
     bit_fields_lengths = [len(grp).bit_length() for grp in grp2tokens]
 
-    enum_values = {w.replace("-", "_").upper(): (token_to_bits(word_to_code(w, grp2tokens), bit_fields_lengths), 0, 0)
+    enum_values = {w.replace("-", "_").upper(): (token_to_bits(word_to_code(w, grp2tokens), bit_fields_lengths), bitmask_for_group(grp2tokens, word_to_groups(w, grp2tokens)), 0)
          for w in arbo.values}
+    #NOTE add also single-token natures, those are not strictly values, but useful to have as individual tokens, for tests
     enum_values.update({tok.upper(): (token_to_bits(word_to_code(tok, grp2tokens), bit_fields_lengths), bitmask_for_group(grp2tokens, token_to_grp(tok, grp2tokens)), offset_for_group(grp2tokens, token_to_grp(tok, grp2tokens)))  
         for tok in arbo.tokens})
     # create NatTok as before
     
-    class NatTokBase(IntEnum):
+    class TokEnumBase(IntEnum):
         def __new__(cls, *args):
             # args is typically (value,) or (value, extra)
             value = args[0]
@@ -125,81 +127,37 @@ def test_arbo(values_set):
             obj = int.__new__(cls, value)
             obj._value_ = value
             # attach any extra attributes you need
-            obj.bitmask = args[1]
-            obj.offset = args[2]
+            obj.bitmask = args[1] if len(args) > 1 else 0
+            obj.offset = args[2] if len(args) > 2 else 0
 
             return obj
 
-        def __init__(self, *args,**kwargs):
-            print(f"init NatTokBase {args} {kwargs.keys()}")
-            super().__init__(*args,**kwargs)
-
-        def is_a_tok(self, tok)->bool:
+        def has_token(self, tok)->bool:
             return self.value & tok.bitmask == tok.value
     
 
-    NatTok = NatTokBase(
+    NatTok = TokEnumBase(
         "NatTok",
-        enum_values)
+        enum_values
+        )
+
     for x in NatTok:
-        print(f"{x.name} {x.is_a_tok(NatTok.ADJ)} bitmask:{x.bitmask:b} offset:{x.offset}")
-    NatTok.bit_fields_lengths = bit_fields_lengths
-    # add extra fields to each member
-    for nat in NatTok:
-        nat.name_str = nat.name.replace("_","-").lower()                     # original token name
-        # nat.code = word_to_code(nat.name_str, grp2tokens)                 # tuple code
-        if len(nat.name_str.split("-")) == 1:
-            grp = token_to_grp(nat.name_str, grp2tokens)
-            # nat.group = my_token_to_grp(nat.name_str, grp2tokens)  # group id
-            nat.bitmask = bitmask_for_group(grp2tokens, grp)
-            nat.offset = offset_for_group(grp2tokens, grp)
-        # nat.tokens = grp2tokens[ my_token_to_grp(nat.name, grp2tokens) ]  # token list for this group
-        # nat.is_adj = ("adj" in nat.name)                             # any flag you want
-    
-    bitmask_for_group(grp2tokens,token_to_grp("adj",grp2tokens))
-    
-    def is_nom(nature:NatTok)->bool:
-        return nature.is_a_tok(NatTok.NOM)
-        return nature.value & NatTok.NOM.bitmask == NatTok.NOM.value
-        return nature.value & bitmask_for_group(grp2tokens,NatTok.NOM.group) == NatTok.NOM.value
-    
-    def is_verb(nature:NatTok)->bool:
-        return nature.is_a_tok(NatTok.VERB)
-        return nature.value & NatTok.VERB.bitmask == NatTok.VERB.value
-        return nature.value & bitmask_for_group(grp2tokens,NatTok.VERB.group) == NatTok.VERB.value
+        print(f"{x.name} {x.has_token(NatTok.ADJ)} bitmask:{x.bitmask:b} offset:{x.offset}")
 
-
-    def is_adj(nature:NatTok)->bool:
-        return nature.is_a_tok(NatTok.ADJ)
-        return nature.value & NatTok.ADJ.bitmask == NatTok.ADJ.value
-        return nature.value & bitmask_for_group(grp2tokens,NatTok.ADJ.group) == NatTok.ADJ.value
-
-
-    def is_symb(nature:NatTok)->bool:
-        return nature.is_a_tok(NatTok.SYMB)
-        return nature.value & NatTok.SYMB.bitmask == NatTok.SYMB.value
-        return nature.value & bitmask_for_group(grp2tokens,NatTok.SYMB.group) == NatTok.SYMB.value
-        # return nature.value & NatTok.ADJ.value == NatTok.ADJ.value
-    
-    def is_flex(nature:NatTok)->bool:
-        return nature.is_a_tok(NatTok.FLEX)
-        return nature.value & NatTok.FLEX.bitmask == NatTok.FLEX.value
 
     for n in NatTok:
         assert n.value == token_to_bits(word_to_code(n.name.replace("_","-").lower(), grp2tokens), bit_fields_lengths)
         # print(f"nature {n} code {get_code(n.name.replace('_','-').lower(), grp2tokens)} bin {bin(n.value)} bits {n.value:0{sum(bit_fields_lengths)}b}")
-        is_symb(n)
-        for tok, is_tok_func in [(NatTok.SYMB,is_symb), (NatTok.ADJ, is_adj), (NatTok.VERB, is_verb), (NatTok.FLEX, is_flex), (NatTok.NOM, is_nom)]:
+
+        for tok in [NatTok.SYMB, NatTok.ADJ, NatTok.VERB, NatTok.FLEX, NatTok.NOM, NatTok.FLEX_ADJ]:
             if tok==NatTok.NOM and "PRONOM" in n.name or "ONOMA" in n.name or "PRÉNOM" in n.name:
-                print(f"skip nom/pronom/prénom/onoma test for {n} ")
+                print(f"skip nom/pronom/prénom/onoma test for {n.name} ")
                 continue
             if tok.name in n.name:
-                assert n.is_a_tok(tok), f"nature {n} {n.name} incorrectly identified as NOT {tok.name} {tok.value}"
-                assert is_tok_func(n), f"nature {n} {n.name} incorrectly identified as NOT {tok.name} {tok.value}"
+                assert n.has_token(tok), f"nature {n} {n.name} incorrectly identified as NOT {tok.name} {tok.value}"
             else:
-                assert not n.is_a_tok(tok), f"nature {n} {n.name} incorrectly identified as {tok.name} {tok.value}"
-                assert not is_tok_func(n), f"nature {n} {n.name} incorrectly identified as {tok.name} {tok.value}"
-        
+                assert not n.has_token(tok), f"nature {n} {n.name} incorrectly identified as {tok.name} {tok.value}"
+                
     return NatTok
 
 natures = {'pronom-rel', 'flex-verb', 'nom', 'flex-art-déf', 'var-typo', 'adj-dém', 'part', 'flex-art-indéf', 'flex-adj-pos', 'adj-indéf', 'prénom', 'nom-pr', 'pronom-pers', 'adj', 'adv', 'verb', 'art-part', 'flex-adj', 'flex-adj-indéf', 'onoma', 'adj-int', 'conj-coord', 'flex-adj-dém', 'adv-int', 'flex-nom', 'interj', 'symb', 'flex-pronom-dém', 'adv-rel', 'suf', 'pronom-dém', 'flex-pronom-rel', 'flex-pronom-pers', 'phr', 'flex-prép', 'lettre', 'adj-num', 'pronom-int', 'prép', 'flex-pronom-int', 'adj-rel', 'adj-pos', 'flex-adv', 'pronom', 'nom-fam', 'pronom-indéf', 'flex-pronom-indéf', 'art-indéf', 'flex-adj-int', 'art-déf', 'conj'}
